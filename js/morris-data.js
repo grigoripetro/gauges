960366d29b4c3fbae00783fb2f0c8dc6d1a3f42a
grigoripetro <39214560+grigoripetro@users.noreply.github.com>
2018-07-03 17:28:02 +0300

    Update morris-data.js

--- js/morris-data.js
+++ js/morris-data.js
@@ -80,36 +80,29 @@ $(function() {
         element: 'morris-bar-chart',
         data: [{
             y: '2006',
-            a: 100,
-            b: 90
+            a: 100
         }, {
             y: '2007',
-            a: 75,
-            b: 65
+            a: 75
         }, {
             y: '2008',
-            a: 50,
-            b: 40
+            a: -50
         }, {
             y: '2009',
-            a: 75,
-            b: 65
+            a: 75
         }, {
             y: '2010',
-            a: 50,
-            b: 40
+            a: 50
         }, {
             y: '2011',
-            a: 75,
-            b: 65
+            a: 75
         }, {
             y: '2012',
-            a: 100,
-            b: 90
+            a: 100
         }],
         xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
+        ykeys: ['a'],
+        labels: ['Series A'],
         hideHover: 'auto',
         resize: true
     });
